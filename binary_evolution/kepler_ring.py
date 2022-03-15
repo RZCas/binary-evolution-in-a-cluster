fixed the calculation of epsilon_gr and also added the missing factor of 2 in the definition of Hernquist potentia

binary_evolution/kepler_ring.py
@@ -836,9 +836,9 @@ class KeplerRing:
         # print("outer orbit interpolation: ", time.time()-t_0, file=time_file, flush=True)
         
         # plot the outer orbit 
-        outer_orbit_file = open ("output/test.txt", 'w+')
-        for t_i in t: #np.linspace(0, t[-1], len(t)):
-            print(t_i, r(t_i), file = outer_orbit_file, flush=True)
+        # outer_orbit_file = open ("output/test.txt", 'w+')
+        # for t_i in t: #np.linspace(0, t[-1], len(t)):
+        #     print(t_i, r(t_i), file = outer_orbit_file, flush=True)
 
         # print("tidal timescale calculation started")
         # Determine if tidal effects are negligible compared to GR precession
@@ -846,15 +846,17 @@ class KeplerRing:
         t_0 = time.time()
         ttensor = TidalTensor(pot)
         # tt_diag = [np.diag(ttensor(x, y, z)) for x, y, z in np.transpose(r(t))[::100]]#zip(xs, ys, zs)]
-        t_array = t[:min(2*self.points_per_period,len(t))]
+        t_array = t[:min(200*self.points_per_period,len(t))]
         # print(t_array)
         tt_diag = [np.diag(ttensor(x, y, z)) for x, y, z in np.transpose(r(t_array))]
         tt_mean = np.mean(tt_diag, axis=0)
         txx, tyy, tzz = tuple(tt_mean)
-        tau_tidal_inverse = 3 * self._a**1.5 / 2 / (_G * self._m)**0.5 * (tyy + tzz)
+        # tau_tidal_inverse = 3 * self._a**1.5 / 2 / (_G * self._m)**0.5 * (tyy + tzz)
         self.tidal_time = time.time()-t_0
 
-        print("Gamma = ", self.gamma(pot))#(tyy-txx)/3/(tyy+txx))
+        # print("Gamma = ", self.gamma(pot))
+        self.gamma_value = (tzz-txx)/3/(tzz+txx) 
+        print("Gamma =", self.gamma_value)
         # print("tidal timescale calculation: ", time.time()-t_0, file=time_file, flush=True)
         # print("tidal timescale calculated")
 
@@ -864,14 +866,15 @@ class KeplerRing:
         # print('')
 
         t_0 = time.time()
-        self.gr_ratio = self.tau_omega(self._a, self.ecc()) * tau_tidal_inverse
-        epsilon_gr = 12 / self.gr_ratio
-        print("log_10(epsilon_GR) = ", np.log10(epsilon_gr))
-        print("tau_omega = %.2e" % self.tau_omega(self._a, self.ecc()))
+        # self.gr_ratio = self.tau_omega(self._a, self.ecc()) * tau_tidal_inverse
+        # self.epsilon_gr = 12 / self.gr_ratio
+        self.epsilon_gr = 24 * (_G * self._m)**2 / (_c**2 * (tyy + tzz) * self._a**4)
+        print("epsilon_gr =", self.epsilon_gr)
+        # print("tau_omega = %.2e" % self.tau_omega(self._a, self.ecc()))
         if debug_file!='': 
             whatIsGoingOn = open(debug_file, 'w+')
-            print ('t a e omega i', file=whatIsGoingOn)
-        if (self.gr_ratio>1 or forcePrecise) and not forceApproximate:
+            print ('t a e omega i real_time', file=whatIsGoingOn)
+        if (self.epsilon_gr<1 or forcePrecise) and not forceApproximate:
             # List of derivative functions to sum together
             funcs = []
             if pot is not None:
@@ -886,10 +889,10 @@ class KeplerRing:
             funcs.append(lambda *args: self._probability_increase(tau_0(args[3], np.linalg.norm(args[4]))))
 
             # Combined derivative function
-            def derivatives(time, e, j, a, probability):
-                r_vec = r(time)
-                result = np.sum([f(time, e, j, a, r_vec) for f in funcs], axis=0) 
-                if debug_file!='': print(time, (a*u.pc).to(u.au).value, np.linalg.norm(e), vectors_to_elements(e, j)[3], vectors_to_elements(e, j)[1], file = whatIsGoingOn, flush=True)
+            def derivatives(t, e, j, a, probability):
+                r_vec = r(t)
+                result = np.sum([f(t, e, j, a, r_vec) for f in funcs], axis=0) 
+                if debug_file!='': print(t, (a*u.pc).to(u.au).value, np.linalg.norm(e), vectors_to_elements(e, j)[3], vectors_to_elements(e, j)[1], time.time(), file = whatIsGoingOn, flush=True)
                 return result
 
             self._integrate_eja(t, derivatives, rtol=rtol, atol=atol,
@@ -899,10 +902,10 @@ class KeplerRing:
             funcs.append(lambda *args: self._probability_increase(tau_0(args[0], np.linalg.norm(args[2]))))
             funcs.append(lambda *args: self.derivatives_gr(args[0], args[1])) 
 
-            def derivatives(time, a, e, omega, probability):
-                r_vec = r(time)
+            def derivatives(t, a, e, omega, probability):
+                r_vec = r(t)
                 result = np.sum([f(a, e, r_vec) for f in funcs], axis=0) 
-                if debug_file!='': print(time, (a*u.pc).to(u.au).value, e, omega, file = whatIsGoingOn, flush=True)
+                if debug_file!='': print(t, (a*u.pc).to(u.au).value, e, omega, time.time(), file = whatIsGoingOn, flush=True)
                 return result
 
             self._integrate_gr_dominated(t, derivatives, rtol=rtol, atol=atol, method=ej_method, random_number=random_number)

binary_evolution_with_flybys.py
@@ -210,7 +210,7 @@ def evolve_binary_noenc (input):
 	m_total = input.m_total
 	type = input.potential
 	if type=="Plummer": pot = PlummerPotential(amp=m_total*u.solMass, b=b*u.pc) 
-	elif type=="Hernquist": pot = HernquistPotential(amp=m_total*u.solMass, a=b*u.pc) 
+	elif type=="Hernquist": pot = HernquistPotential(amp=2*m_total*u.solMass, a=b*u.pc) 
 
 	# Compute the correct v_phi at pericentre for the selected eccentricity and potential
 	v_phi = ecc_to_vel(pot, ecc_out, [R, z, 0])
@@ -224,7 +224,6 @@ def evolve_binary_noenc (input):
 	print(0, R, z, 0, 0, 0, v_phi, k.a(), k.m(), k._q, k.ecc(), k.inc(), k.long_asc(), k.arg_peri(), file=output_file, flush=True)
 
 	T = 2*np.pi*(r|units.pc)/sigma_rel(r|units.pc, type, m_total, b)	# approximate outer period
-	print(sigma_rel(r|units.pc, type, m_total, b))
 	n = max(int(input.n*t/(T.value_in(units.yr))), 10)	#number of points used to approximate the outer orbit
 	# n=10
 	ts = np.linspace(0, t, n)
@@ -232,7 +231,9 @@ def evolve_binary_noenc (input):
 	atol= rtol*1e-3 #1e-14
 
 	k.integrate(ts, pot=pot, relativity=True, gw=True, tau_0=lambda *args: tau_0(args[0]|units.pc, k.m()|units.MSun, args[1]|units.pc, 50, type, m_total, b).value_in(units.yr), random_number=1e10, rtol=rtol, atol=atol, forcePrecise=input.forcePrecise, forceApproximate=input.forceApproximate, debug_file=input.output_file_2, points_per_period=input.n)
-	print('gr_ratio =', k.gr_ratio, ', t =', t, file=output_file)
+	print('epsilon_gr =', k.epsilon_gr, ', Gamma =', k.gamma_value, ', t =', t, file=output_file)
+	print(, ', t =', t, file=output_file)
+	# print('gamma =', k.gamma(pot), file=output_file)
 	print('da de di dOmega domega', file=output_file)
 	if k.merger: print('merger at', k.t_fin, file=output_file, flush=True)
 	else: print(k.t_fin, k.a_fin-a_in, k.ecc_fin-ecc, k.inc_fin-inc, k.long_asc_fin-long_asc, k.arg_peri_fin-arg_peri, k.outer_integration_time, k.tidal_time, k.inner_integration_time, file=output_file, flush=True)

calculate.py
@@ -4,17 +4,23 @@ gamma = 0.42
 e = 0.5
 i = 89.9 * np.pi/180
 omega = 91 * np.pi/180
-theta = (1-e**2)*np.cos(i)**2
+eps_gr = 0.0691200754099
 
-eps_gr = 10**-1.5
+def e_max (e, i, omega, eps_gr):
 
-D = e**2 * (1 + 10*gamma/(1-5*gamma) * np.sin(i)**2*np.sin(omega)**2) - eps_gr/(3*(1-5*gamma)*np.sqrt(1-e**2))
-sigma = (1+5*gamma)/2 + 5*gamma*theta + (5*gamma-1)/2*D
-jplus2 = (sigma + np.sqrt(sigma**2 - 10*gamma*theta*(1+5*gamma))) / (1+5*gamma)
-jminus2 = (sigma - np.sqrt(sigma**2 - 10*gamma*theta*(1+5*gamma))) / (1+5*gamma)
-eps_weak = 6*(1+5*gamma)*jplus2*np.sqrt(jminus2) #10**-1.6
-eps_strong = 3*(1+5*gamma) #10
-jmin = (eps_gr + np.sqrt(eps_gr**2+eps_weak**2)) / (2*jplus2*eps_strong)
+	# returns log_10(1-e_max)
 
-answer = np.log10 (1-np.sqrt(1-jmin**2))
-print(answer)
+	theta = (1-e**2)*np.cos(i)**2
+	D = e**2 * (1 + 10*gamma/(1-5*gamma) * np.sin(i)**2*np.sin(omega)**2) - eps_gr/(3*(1-5*gamma)*np.sqrt(1-e**2))
+	sigma = (1+5*gamma)/2 + 5*gamma*theta + (5*gamma-1)/2*D
+	jplus2 = (sigma + np.sqrt(sigma**2 - 10*gamma*theta*(1+5*gamma))) / (1+5*gamma)
+	jminus2 = (sigma - np.sqrt(sigma**2 - 10*gamma*theta*(1+5*gamma))) / (1+5*gamma)
+	eps_weak = 6*(1+5*gamma)*jplus2*np.sqrt(jminus2) #10**-1.6
+	eps_strong = 3*(1+5*gamma) #10
+	jmin = (eps_gr + np.sqrt(eps_gr**2+eps_weak**2)) / (2*jplus2*eps_strong)
+
+	return np.log10 (1-np.sqrt(1-jmin**2))
+
+print (e_max(e, i, omega, eps_gr))
+
+# cluster4 - ok

main.py
@@ -6,16 +6,18 @@ from amuse.lab import units
 _G = constants.G.to(u.pc**3/u.solMass/u.yr**2).value
 _c = constants.c.to(u.pc/u.yr).value
 
-t = 1e6
+t = 1.1e10
 
 # Inner binary parameters
-a_in = 250              # Semi-major axis in AU
-ecc = 0.6            	# Eccentricity
-inc = 89.8 * np.pi/180  # Inclination with respect to the z-axis
+a_in = 49              # Semi-major axis in AU
+ecc = 0.5            	# Eccentricity
+inc = 89.9 * np.pi/180#  #  # Inclination with respect to the z-axis
 long_asc = 0            # Longitude of the ascending node
-arg_peri = 91.0 * np.pi/180    # Arugment of pericentre
-m1 = 10
-m2 = 10
+arg_peri = 91.0 * np.pi/180# #    # Arugment of pericentre
+m_tot = 2.8
+q = 1
+m1 = m_tot / (1+q)
+m2 = m_tot * q / (1+q)
 
 # print((a_in*(1-ecc)*u.au).to(u.pc), flush=True)
 
@@ -24,12 +26,12 @@ ecc_out = 0.2/3.2         # Outer orbit eccentricity
 inc_out = 0             # Outer orbit inclination
 a_out = 1.6        # Outer semi-major axis in pc
 
-folder = 'output/chris_thesis/'
+folder = 'output/chris_thesis_corrected/'#'output/noenc_test/cluster/'##
 
 forcePrecise = False
 forceApproximate = False
-output_file = folder + 'a_in='+str(a_in)+'_e_in='+str(ecc)+'_test.txt'
-output_file_2 = ''#folder + 'a_in='+str(a_in)+'_e_in='+str(ecc)+'_precise_evolution.txt'
+output_file = folder+'a_in='+str(a_in)+'_e_in='+str(ecc)+'.txt'#folder + '0test.txt'###
+output_file_2 = folder + 'a_in='+str(a_in)+'_e_in='+str(ecc)+'_evolution.txt'
 
 # forcePrecise = False
 # forceApproximate = not forcePrecise
@@ -58,6 +60,6 @@ input = inputParameters(t=t, a_out=a_out, e_out=ecc_out, inc_out=inc_out, m1=m1,
 	resume=False, 
 	includeEncounters=True, 
 	includeWeakEncounters=True, 
-	n=1000)
+	n=30)
 # evolve_binary(input)
 evolve_binary_noenc(input)